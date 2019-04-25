40968b6b55fc646acfed2aa596a705ef2b33f4e9
Noe <VaescoNoe@gmail.com>
2019-04-25 00:18:27 -0500

    POO

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System;
+
+namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangulo r = new Rectangulo();
+            r.BaseRectangulo = 4.0;
+            r.AlturaRectangulo = 3.0;
+            r.Color = "Rojo";
+ 
+            Console.WriteLine(r.calcularArea());
+            Console.WriteLine(r.calcularPerimetro());
+            
+
+            // Deconstructor 4.7.2
+            (var baseRect, var alturaRect) = r;
+
+            Console.WriteLine(baseRect);
+            Console.WriteLine(alturaRect);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Rectangulo.cs
@@ -0,0 +1,64 @@
+﻿
+
+namespace POO
+{
+    class Rectangulo
+    {
+        private double _baseRectangulo;
+        public double BaseRectangulo
+                {
+                get { return _baseRectangulo; }
+                set { _baseRectangulo = value; }
+                }
+
+
+        private double _alturaRectangulo;
+        public double AlturaRectangulo
+        {
+                get { return _alturaRectangulo; }
+                set { _alturaRectangulo = value; }
+            }
+        private string _color;
+        public string Color
+            {
+                get { return _color; }
+                set { _color = value; }
+            }
+
+        public Rectangulo()
+        {
+            _baseRectangulo = 0;
+            _alturaRectangulo = 0;
+            _color = "Negro";
+        }
+
+        public Rectangulo(double baseRectangulo, double alturaRectangulo, string color)
+        {
+            _baseRectangulo = baseRectangulo;
+            _alturaRectangulo = alturaRectangulo;
+            _color = color;
+        }
+
+        public Rectangulo(string color, double baseRectangulo, double alturaRectangulo)
+        {
+            _baseRectangulo = baseRectangulo;
+            _alturaRectangulo = alturaRectangulo;
+            _color = color;
+        }
+
+        public double calcularArea()
+        {
+            return _baseRectangulo * _alturaRectangulo;
+        }
+
+        public double calcularPerimetro()
+        {
+            return (_baseRectangulo + _alturaRectangulo) *2;
+        }
+
+        public void Deconstruct(out double baseRect, out double alturaRect) {
+            baseRect = _baseRectangulo;
+            alturaRect = _alturaRectangulo;
+        }
+    }
+}
